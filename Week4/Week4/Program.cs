252ab2c3671bc10673bf07dc90ba6bbb498d3bd1
YerkezhanSeitbekova <eseitbekova@gmail.com>
2017-02-07 14:19:17 +0600

    Add files via upload

---
+++ Week4/Week4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+
+
+namespace Week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //serialize();
+            deserialize();
+            Console.ReadKey();
+            
+        }
+        static void serialize()
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student("Ali", 19);
+            students[1] = new Student("Bota", 18);
+            students[2] = new Student("Erik", 20);
+            students[3] = new Student("Beibut", 17);
+            FileStream sW = File.Create("Data.txt");
+            //SoapFormatter sf = new SoapFormatter();
+            BinaryFormatter binF = new BinaryFormatter();
+            Student s = new Student("Alina", 30);
+            binF.Serialize(sW, students);
+            binF.Serialize(sW, s);
+        }
+        static void deserialize()
+        {
+            Student[] students = new Student[5];
+            
+            FileStream sW = File.OpenRead("Data.txt");
+            //SoapFormatter sf = new SoapFormatter();
+            BinaryFormatter binF = new BinaryFormatter();
+            Student s; 
+            students = (Student[])binF.Deserialize(sW);
+            s = (Student)binF.Deserialize(sW);
+            foreach (Student ss in students)
+            {
+                Console.WriteLine(ss);
+            }
+            Console.WriteLine(s);
+
+            sW.Close();
+            Console.ReadKey();
+        }
+    }
+   
+}
---
+++ Week4/Week4/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4
+{   
+    [Serializable]
+    class Student
+    {
+        public string name;
+        public int age;
+        public double gpa=2.0;
+        public Student(string n, int a)
+        {
+            name = n;
+            age = a;
+        }
+        public override string ToString()
+        {
+            return name + " " + age + " " + gpa; ;
+        }
+    }
+}
